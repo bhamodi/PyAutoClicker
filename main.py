e469a116db933504b4b8dfc8266d49df23c18e67
Baraa <bhamodi@uwaterloo.ca>
2014-11-24 00:38:57 -0800

    Now supports clicking for a certain amount of clicks.

--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 
 # Imports
 import win32api, win32con
+import time
+import math
 
-# Main
+# Define Click
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-click(10,10)
+
+# Main Logic
+x = 0;
+while (x < 5):
+    time.sleep(2)
+    a, b = win32api.GetCursorPos()
+    click(a, b)
+    x = x + 1
